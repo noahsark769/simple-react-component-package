deb656b1f04197d50a05e42ad8212ef4c7a4010f
Noah Gilmore <noah.w.gilmore@gmail.com>
2021-11-06 16:28:30 -0700

    Add files and build output

---
+++ SimpleComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default SimpleComponent = (props) => {
+    return <div>
+        Hey, this is the SimpleComponent. props.text: ({props.text})
+    </div>
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+import SimpleComponent from "./SimpleComponent";
